5edd4da9522a370c5a935435c4d312e9a55bfcd2
moreshwar <moreshwarpowar12@gmail.com>
2022-11-02 01:21:08 +0530

    this is my first code push in git fb

--- mongotest.py
+++ mongotest.py
@@ -4,6 +4,10 @@ db = client.test
 print(db)
 
 d={"name":"moreshwar","email":"moreshwarpowar12@gmail.com","surname":"powar"}
+d={"name":"moreshwar","email":"moreshwarpowar12@gmail.com","surname":"powar"}
+d={"name":"moreshwar","email":"moreshwarpowar12@gmail.com","surname":"powar"}
+d={"name":"moreshwar","email":"moreshwarpowar12@gmail.com","surname":"powar"}
+
 db1=client['mongotest']
 coll=db1['test1']
 coll.insert_one(d)
